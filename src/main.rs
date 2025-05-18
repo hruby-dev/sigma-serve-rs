783def902b8ea638f2de324aa2d021360f0c24a4
Maya <maya@nyxi.sk>
2025-05-19 00:08:59 +0200

    feat: implement clap-based config, allow user to specify bind address, implement more sane path handling

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,50 @@
 use std::{
-    env, fs,
-    io::{BufRead, BufReader, Write},
+    fs,
+    io::{self, BufRead, BufReader, Write},
     net::{TcpListener, TcpStream},
+    path::{self, PathBuf},
     sync::Arc,
 };
 
-use log::{error, info};
+use clap::Parser;
+use log::{debug, error, info};
+
+#[derive(Parser)]
+pub struct Args {
+    /// which directory or file to serve
+    root: PathBuf,
+
+    #[arg(short, long, default_value = "localhost:8080")]
+    /// what address and port pair to listen on
+    bind: String,
+}
 
 fn main() -> std::io::Result<()> {
     let _ = simplelog::TermLogger::init(
         if cfg!(debug_assertions) {
-            log::LevelFilter::Info
-        } else {
             log::LevelFilter::Debug
+        } else {
+            log::LevelFilter::Info
         },
         simplelog::Config::default(),
         simplelog::TerminalMode::Mixed,
         simplelog::ColorChoice::Auto,
     );
 
-    let listener = TcpListener::bind("127.0.0.1:8081")?;
-    info!("sigma-serve-rs started serving files on http://127.0.0.1:8081");
+    let mut args = Args::parse();
+    args.root = fs::canonicalize(&args.root)?;
+    let args = Arc::new(args);
 
-    let args: Vec<String> = env::args().collect();
-    if args.len() < 2 {
-        eprintln!("Usage: {} <root_directory>", args[0]);
-        std::process::exit(1);
-    }
-    let root_dir = Arc::new(args[1].clone());
+    let listener = TcpListener::bind(&args.bind)?;
+    info!("sigma-serve-rs started serving files on {}", args.bind);
 
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
-                let root_dir = Arc::clone(&root_dir);
+                let args = Arc::clone(&args);
                 std::thread::spawn(move || {
-                    if let Err(e) = handle_client(stream, &root_dir) {
-                        error!("client handler errored: {}", e);
+                    if let Err(e) = handle_client(stream, &args) {
+                        error!("client handler error: {}", e);
                     }
                 });
             }
@@ -45,7 +54,7 @@ fn main() -> std::io::Result<()> {
     Ok(())
 }
 
-fn handle_client(mut stream: TcpStream, root_dir: &str) -> std::io::Result<()> {
+fn handle_client(mut stream: TcpStream, args: &Args) -> std::io::Result<()> {
     let buf_reader = BufReader::new(&stream);
     let request_line = match buf_reader.lines().next().transpose()? {
         Some(line) => line,
@@ -65,26 +74,32 @@ fn handle_client(mut stream: TcpStream, root_dir: &str) -> std::io::Result<()> {
     }
 
     let requested = if path == "/" {
-        "index.html".to_string()
+        PathBuf::from("index.html")
     } else {
-        format!("{}.html", path.trim_start_matches('/'))
+        PathBuf::from(format!("{}.html", path.trim_start_matches('/')))
     };
 
-    if requested.contains("..") {
+    let full_path = match fs::canonicalize(args.root.join(requested)) {
+        Ok(full_path) => full_path,
+        Err(e) if e.kind() == io::ErrorKind::NotFound => return send_not_found(&mut stream, args),
+        Err(e) => return Err(e),
+    };
+
+    if !full_path.starts_with(&args.root) {
         return send_response(&mut stream, "HTTP/1.1 403 FORBIDDEN", "Forbidden");
     }
 
-    let full_path = format!("{}/{}", root_dir, requested);
-
     match fs::read_to_string(&full_path) {
         Ok(contents) => send_response(&mut stream, "HTTP/1.1 200 OK", &contents),
-        Err(_) => {
-            let fallback_path = format!("{}/404.html", root_dir);
-            let fallback =
-                fs::read_to_string(&fallback_path).unwrap_or_else(|_| "404 Not Found".to_string());
-            send_response(&mut stream, "HTTP/1.1 404 NOT FOUND", &fallback)
-        }
+        Err(_) => send_not_found(&mut stream, args)
     }
+}
+
+fn send_not_found(stream: &mut TcpStream, args: &Args) -> std::io::Result<()> {
+    let fallback_path = args.root.join("404.html");
+    let fallback =
+        fs::read_to_string(&fallback_path).unwrap_or_else(|_| "404 Not Found".to_string());
+    send_response(stream, "HTTP/1.1 404 NOT FOUND", &fallback)
 }
 
 fn send_response(stream: &mut TcpStream, status_line: &str, body: &str) -> std::io::Result<()> {
